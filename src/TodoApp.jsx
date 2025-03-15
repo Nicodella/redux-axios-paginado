46f2cbf5e7e6571f699b14180b4ef7f525da501d
Rodrigo <r.nicodella12@gmail.com>
2025-03-15 13:20:45 -0300

    avances rtk

---
+++ src/TodoApp.jsx
@@ -0,0 +1,18 @@
+
+
+export const TodoApp = () => {
+  return (
+    <>
+        <h1>TodoApp</h1>
+        <hr/>
+        <h4>isLoading</h4>
+
+        <pre>...</pre>
+
+        <button>
+            Next Todo
+        </button>
+    </>
+    
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import { Provider } from 'react-redux'
 import {store} from './store'
 import './index.css'
 import { PokemonApp } from './PokemonApp.jsx'
+import { TodoApp } from './TodoApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-    <PokemonApp />
+    <TodoApp />
     </Provider>
   </StrictMode>,
 )
